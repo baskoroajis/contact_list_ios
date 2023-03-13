3d2097f6902357782cdbdde30a50ca2d8d323071
Baskoro <>
2023-03-13 07:36:29 +0700

    first commit

---
+++ TrinityIOS/TrinityIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrinityIOSApp.swift
+//  TrinityIOS
+//
+//  Created by Baskoro on 13/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TrinityIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
